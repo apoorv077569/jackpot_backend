ac487b7852cefdb1527774a98df62b7df93e4bf3
Apoorv Rathore <0211ite094@niet.co.in>
2025-11-13 15:26:08 +0530

    Update.env and src/app.js files with new MongoDB URI and JWT secret key

--- src/app.js
+++ src/app.js
@@ -11,10 +11,9 @@ const app = express();
 app.use(express.json());
 
 app.use('/static',express.static("public"));
-app.use('/api/game', gameRoute);
 app.use("/assets", express.static(path.join(path.resolve(), "assets")));
 app.use("/api/users",userRoutes);
-app.use("/api/game/", gameRoute);
+app.use("/api/game", gameRoute);
 
 app.get("/", (req, res) => {
   res.send("Jackpot Game API is running!");
